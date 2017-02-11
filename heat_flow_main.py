d12cfa37d2ca31134259ee7416e2b092e5d3c62c
argolomb <argolo.mb@gmail.com>
2017-02-11 19:30:59 -0300

    Adicionando projeto

---
+++ heat_flow_main.py
@@ -0,0 +1,16 @@
+import sys
+
+from PyQt5 import QtGui, QtCore, QtWidgets
+from heat_flow_gui import Ui_MainWindow
+
+def Heat_Flow_Gui(Ui_MainWindow):
+    def __init__(self,MainWindow):
+        Ui_MainWindow.__init__(self)
+        self.setupUi(MainWindow)
+
+if __name__== "__main__":
+    app=QtWidgets.QApplication(sys.argv)
+    MainWindow=QtWidgets.QMainWindow()
+    prog =Heat_Flow_Gui(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
